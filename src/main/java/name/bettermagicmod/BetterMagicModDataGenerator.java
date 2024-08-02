4dee0835124e0ac09151070c4a59ea0cd0e98d90
ADG2024 <imasb147@gmail.com>
2024-08-02 23:26:01 +0800

    Initial commit

---
+++ src/main/java/name/bettermagicmod/BetterMagicModDataGenerator.java
@@ -0,0 +1,11 @@
+package name.bettermagicmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class BetterMagicModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
